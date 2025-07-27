7a20822b68a27c6e6f91926706798c91895fa518
ambermender <ambermender@example.com>
2025-07-27 21:27:12 +0300

    Add CORS support for browser requests

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  // CORS'u etkinleştir
+  app.enableCors({
+    origin: true, // Tüm origin'lere izin ver
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  
+  await app.listen(3000);
 }
 bootstrap();
